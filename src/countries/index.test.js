cce8131e892e70d476e1d0caa641b01a089e5718
Andres2D <andres2d1997.1206@gmail.com>
2022-10-26 11:49:20 -0500

    Add basic tests

---
+++ src/countries/index.test.js
@@ -0,0 +1,12 @@
+import { getFlagSvg } from './index.js';
+
+test('Should return the flag with partial argument', () => {
+  expect(getFlagSvg('Col', false))
+    .toEqual({name: "Colombia",flag: "https://flagcdn.com/co.svg"})
+});
+
+test('Should return the flag with full argument', () => {
+  expect(getFlagSvg('Colombia', true))
+    .toEqual({name: "Colombia",flag: "https://flagcdn.com/co.svg"})
+});
+
