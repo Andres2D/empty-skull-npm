9f08e239655f32cccc81b15a8a1b2cd76c808552
Andres2D <andres2d1997.1206@gmail.com>
2022-10-26 12:15:10 -0500

    Add function description

--- src/countries/index.js
+++ src/countries/index.js
@@ -1,5 +1,11 @@
 import { flagsCollection } from './flags.js';
 
+/**
+ * 
+ * @param {*} name full or partial country name
+ * @param {*} isFull flag to know if the name is full or partial
+ * @returns An object with the name and the flag svg link
+ */
 export const getFlagSvg = (name, isFull) => {
   if(isFull) {
     return [...flagsCollection].filter(f => f.name.toLocaleLowerCase() === name.toLocaleLowerCase())[0];
